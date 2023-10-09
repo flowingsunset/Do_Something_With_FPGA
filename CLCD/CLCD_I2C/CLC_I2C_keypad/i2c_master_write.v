030a88cb808e185ed2c04f995febe318fba05a3a
flowingsunset <40950645+flowingsunset@users.noreply.github.com>
2023-10-09 18:17:12 +0900

    I2C Read, display DDRAM address, auto line change

--- CLCD/CLCD_I2C/CLC_I2C_keypad/i2c_master_write.v
+++ CLCD/CLCD_I2C/CLC_I2C_keypad/i2c_master_write.v
@@ -1,13 +1,14 @@
-module i2c_master_write (
+module i2c_master (
     input wire clk,
     input wire reset_p,
     input wire [6:0] i_addr,
     input wire [7:0] i_data,
     input wire i_RW,
     input wire i_valid,
-    output wire o_sda,
+    inout wire o_sda,
     output wire o_scl,
-    output wire o_busy
+    output wire o_busy,
+    output reg [7:0] r_receive
 );
 
 //no ack and no read
@@ -31,19 +32,28 @@ localparam S_RW = 9;
 //if slave understand sda == 0 else restart communicatinon
 localparam S_ACK1 = 10;
 //sending 8 bit i2c data
-localparam S_DATA7 = 11;
-localparam S_DATA6 = 12;
-localparam S_DATA5 = 13;
-localparam S_DATA4 = 14;
-localparam S_DATA3 = 15;
-localparam S_DATA2 = 16;
-localparam S_DATA1 = 17;
-localparam S_DATA0 = 18;
+localparam S_WRITE7 = 11;
+localparam S_WRITE6 = 12;
+localparam S_WRITE5 = 13;
+localparam S_WRITE4 = 14;
+localparam S_WRITE3 = 15;
+localparam S_WRITE2 = 16;
+localparam S_WRITE1 = 17;
+localparam S_WRITE0 = 18;
+//reading 8 bit i2c data
+localparam S_READ7 = 19;
+localparam S_READ6 = 20;
+localparam S_READ5 = 21;
+localparam S_READ4 = 22;
+localparam S_READ3 = 23;
+localparam S_READ2 = 24;
+localparam S_READ1 = 25;
+localparam S_READ0 = 26;
 //if slave understand write, sda == 0 else restart communication
 //if master understand read, sda == 1 else restart communication
-localparam S_ACK2 = 19;
+localparam S_ACK2 = 27;
 //set SDA low to high to end i2c communication
-localparam S_STOP = 20;
+localparam S_STOP = 28;
 
 //state register
 reg [6:0]r_state, r_next_state;
@@ -55,7 +65,7 @@ reg r_sda, r_scl;
 reg [1:0] r_cnt_i2c;
 reg r_RW;
 
-assign o_sda = r_sda;
+assign o_sda = r_sda ? 1'hz : 0;
 assign o_scl = r_scl;
 assign o_busy = (r_state == S_IDLE) ? 0 : 1;
 always @(negedge clk, posedge reset_p ) begin
@@ -72,6 +82,7 @@ always @(posedge clk, posedge reset_p) begin
         r_sda <= 1;
         r_cnt_i2c <= 0;
         r_RW <= 0;
+        r_receive <= 0;
     end else begin
         case (r_state)
             S_IDLE: begin
@@ -89,11 +100,11 @@ always @(posedge clk, posedge reset_p) begin
                 case (r_cnt_i2c)
                     0: begin
                         r_scl <= 0;
-                        r_sda <= 1'hz;
+                        r_sda <= 1;
                     end
                     1: begin
                         r_scl <= 1;
-                        r_sda <= 1'hz;
+                        r_sda <= 1;
                     end
                     2: begin
                         r_scl <= 1;
@@ -291,7 +302,10 @@ always @(posedge clk, posedge reset_p) begin
             end
             S_ACK1: begin
                 r_cnt_i2c <= r_cnt_i2c + 1;
-                if (r_cnt_i2c >=3)r_next_state <= S_DATA7;
+                if (r_cnt_i2c >=3) begin
+                    if(r_RW == 0)r_next_state <= S_WRITE7;
+                    if(r_RW == 1)r_next_state <= S_READ7;
+                end
                 else r_next_state <= S_ACK1;
                 case (r_cnt_i2c)
                     0: begin
@@ -312,10 +326,10 @@ always @(posedge clk, posedge reset_p) begin
                     end
                 endcase
             end
-            S_DATA7: begin
+            S_WRITE7: begin
                 r_cnt_i2c <= r_cnt_i2c + 1;
-                if (r_cnt_i2c >=3)r_next_state <= S_DATA6;
-                else r_next_state <= S_DATA7;
+                if (r_cnt_i2c >=3)r_next_state <= S_WRITE6;
+                else r_next_state <= S_WRITE7;
                 case (r_cnt_i2c)
                     0: begin
                         r_scl <= 0;
@@ -335,10 +349,10 @@ always @(posedge clk, posedge reset_p) begin
                     end
                 endcase
             end
-            S_DATA6: begin
+            S_WRITE6: begin
                 r_cnt_i2c <= r_cnt_i2c + 1;
-                if (r_cnt_i2c >=3)r_next_state <= S_DATA5;
-                else r_next_state <= S_DATA6;
+                if (r_cnt_i2c >=3)r_next_state <= S_WRITE5;
+                else r_next_state <= S_WRITE6;
                 case (r_cnt_i2c)
                     0: begin
                         r_scl <= 0;
@@ -358,10 +372,10 @@ always @(posedge clk, posedge reset_p) begin
                     end
                 endcase
             end
-            S_DATA5: begin
+            S_WRITE5: begin
                 r_cnt_i2c <= r_cnt_i2c + 1;
-                if (r_cnt_i2c >=3)r_next_state <= S_DATA4;
-                else r_next_state <= S_DATA5;
+                if (r_cnt_i2c >=3)r_next_state <= S_WRITE4;
+                else r_next_state <= S_WRITE5;
                 case (r_cnt_i2c)
                     0: begin
                         r_scl <= 0;
@@ -381,10 +395,10 @@ always @(posedge clk, posedge reset_p) begin
                     end
                 endcase
             end
-            S_DATA4: begin
+            S_WRITE4: begin
                 r_cnt_i2c <= r_cnt_i2c + 1;
-                if (r_cnt_i2c >=3)r_next_state <= S_DATA3;
-                else r_next_state <= S_DATA4;
+                if (r_cnt_i2c >=3)r_next_state <= S_WRITE3;
+                else r_next_state <= S_WRITE4;
                 case (r_cnt_i2c)
                     0: begin
                         r_scl <= 0;
@@ -404,10 +418,10 @@ always @(posedge clk, posedge reset_p) begin
                     end
                 endcase
             end
-            S_DATA3: begin
+            S_WRITE3: begin
                 r_cnt_i2c <= r_cnt_i2c + 1;
-                if (r_cnt_i2c >=3)r_next_state <= S_DATA2;
-                else r_next_state <= S_DATA3;
+                if (r_cnt_i2c >=3)r_next_state <= S_WRITE2;
+                else r_next_state <= S_WRITE3;
                 case (r_cnt_i2c)
                     0: begin
                         r_scl <= 0;
@@ -427,10 +441,10 @@ always @(posedge clk, posedge reset_p) begin
                     end
                 endcase
             end
-            S_DATA2: begin
+            S_WRITE2: begin
                 r_cnt_i2c <= r_cnt_i2c + 1;
-                if (r_cnt_i2c >=3)r_next_state <= S_DATA1;
-                else r_next_state <= S_DATA2;
+                if (r_cnt_i2c >=3)r_next_state <= S_WRITE1;
+                else r_next_state <= S_WRITE2;
                 case (r_cnt_i2c)
                     0: begin
                         r_scl <= 0;
@@ -450,10 +464,10 @@ always @(posedge clk, posedge reset_p) begin
                     end
                 endcase
             end
-            S_DATA1: begin
+            S_WRITE1: begin
                 r_cnt_i2c <= r_cnt_i2c + 1;
-                if (r_cnt_i2c >=3)r_next_state <= S_DATA0;
-                else r_next_state <= S_DATA1;
+                if (r_cnt_i2c >=3)r_next_state <= S_WRITE0;
+                else r_next_state <= S_WRITE1;
                 case (r_cnt_i2c)
                     0: begin
                         r_scl <= 0;
@@ -473,10 +487,10 @@ always @(posedge clk, posedge reset_p) begin
                     end
                 endcase
             end
-            S_DATA0: begin
+            S_WRITE0: begin
                 r_cnt_i2c <= r_cnt_i2c + 1;
                 if (r_cnt_i2c >=3)r_next_state <= S_ACK2;
-                else r_next_state <= S_DATA0;
+                else r_next_state <= S_WRITE0;
                 case (r_cnt_i2c)
                     0: begin
                         r_scl <= 0;
@@ -493,6 +507,190 @@ always @(posedge clk, posedge reset_p) begin
                     3: begin
                         r_scl <= 0;
                         r_sda <= r_data[0];
+                    end
+                endcase
+            end
+            S_READ7: begin
+                r_cnt_i2c <= r_cnt_i2c + 1;
+                if (r_cnt_i2c >=3)r_next_state <= S_READ6;
+                else r_next_state <= S_READ7;
+                case (r_cnt_i2c)
+                    0: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
+                    end
+                    1: begin
+                        r_scl <= 1;
+                        r_receive[7] <= o_sda;
+                    end
+                    2: begin
+                        r_scl <= 1;
+                        r_receive[7] <= o_sda;
+                    end
+                    3: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
+                    end
+                endcase
+            end
+            S_READ6: begin
+                r_cnt_i2c <= r_cnt_i2c + 1;
+                if (r_cnt_i2c >=3)r_next_state <= S_READ5;
+                else r_next_state <= S_READ6;
+                case (r_cnt_i2c)
+                    0: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
+                    end
+                    1: begin
+                        r_scl <= 1;
+                        r_receive[6] <= o_sda;
+                    end
+                    2: begin
+                        r_scl <= 1;
+                        r_receive[6] <= o_sda;
+                    end
+                    3: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
+                    end
+                endcase
+            end
+            S_READ5: begin
+                r_cnt_i2c <= r_cnt_i2c + 1;
+                if (r_cnt_i2c >=3)r_next_state <= S_READ4;
+                else r_next_state <= S_READ5;
+                case (r_cnt_i2c)
+                    0: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
+                    end
+                    1: begin
+                        r_scl <= 1;
+                        r_receive[5] <= o_sda;
+                    end
+                    2: begin
+                        r_scl <= 1;
+                        r_receive[5] <= o_sda;
+                    end
+                    3: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
+                    end
+                endcase
+            end
+            S_READ4: begin
+                r_cnt_i2c <= r_cnt_i2c + 1;
+                if (r_cnt_i2c >=3)r_next_state <= S_READ3;
+                else r_next_state <= S_READ4;
+                case (r_cnt_i2c)
+                    0: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
+                    end
+                    1: begin
+                        r_scl <= 1;
+                        r_receive[4] <= o_sda;
+                    end
+                    2: begin
+                        r_scl <= 1;
+                        r_receive[4] <= o_sda;
+                    end
+                    3: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
+                    end
+                endcase
+            end
+            S_READ3: begin
+                r_cnt_i2c <= r_cnt_i2c + 1;
+                if (r_cnt_i2c >=3)r_next_state <= S_READ2;
+                else r_next_state <= S_READ3;
+                case (r_cnt_i2c)
+                    0: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
+                    end
+                    1: begin
+                        r_scl <= 1;
+                        r_receive[3] <= o_sda;
+                    end
+                    2: begin
+                        r_scl <= 1;
+                        r_receive[3] <= o_sda;
+                    end
+                    3: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
+                    end
+                endcase
+            end
+            S_READ2: begin
+                r_cnt_i2c <= r_cnt_i2c + 1;
+                if (r_cnt_i2c >=3)r_next_state <= S_READ1;
+                else r_next_state <= S_READ2;
+                case (r_cnt_i2c)
+                    0: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
+                    end
+                    1: begin
+                        r_scl <= 1;
+                        r_receive[2] <= o_sda;
+                    end
+                    2: begin
+                        r_scl <= 1;
+                        r_receive[2] <= o_sda;
+                    end
+                    3: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
+                    end
+                endcase
+            end
+            S_READ1: begin
+                r_cnt_i2c <= r_cnt_i2c + 1;
+                if (r_cnt_i2c >=3)r_next_state <= S_READ0;
+                else r_next_state <= S_READ1;
+                case (r_cnt_i2c)
+                    0: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
+                    end
+                    1: begin
+                        r_scl <= 1;
+                        r_receive[1] <= o_sda;
+                    end
+                    2: begin
+                        r_scl <= 1;
+                        r_receive[1] <= o_sda;
+                    end
+                    3: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
+                    end
+                endcase
+            end
+            S_READ0: begin
+                r_cnt_i2c <= r_cnt_i2c + 1;
+                if (r_cnt_i2c >=3)r_next_state <= S_ACK2;
+                else r_next_state <= S_READ0;
+                case (r_cnt_i2c)
+                    0: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
+                    end
+                    1: begin
+                        r_scl <= 1;
+                        r_receive[0] <= o_sda;
+                    end
+                    2: begin
+                        r_scl <= 1;
+                        r_receive[0] <= o_sda;
+                    end
+                    3: begin
+                        r_scl <= 0;
+                        r_sda <= 1;
                     end
                 endcase
             end
--- CLCD/CLCD_I2C/CLC_I2C_keypad/i2c_top.v
+++ CLCD/CLCD_I2C/CLC_I2C_keypad/i2c_top.v
@@ -1,14 +1,17 @@
 module i2c_top (
     input wire clk,
     input wire reset_p,
-    output wire o_sda,
+    inout wire o_sda,
     output wire o_scl,
     input wire [3:0] i_col,
-    output wire [3:0] o_row
+    output wire [3:0] o_row,
+    output [3:0] an,
+    output [6:0] seg
 );
     wire [6:0] w_addr;
     wire [7:0] w_data;
     wire w_busy, w_RW, w_valid;
+    wire [7:0] w_receive;
 
     wire [6:0] w_addr_key;
     wire [7:0] w_data_key;
@@ -19,9 +22,10 @@ module i2c_top (
     wire w_RW_reg, w_valid_reg;
 
     wire [3:0] w_data_k2k;
+
+    wire [7:0] w_value;
+
     
-    //assign w_sda_e = (w_sda == 1'bz) ? 1 : 0;
-    //assign w_scl_e = (w_scl == 1'bz) ? 1 : 0;
     reg r_clk_400khz;
     reg [6:0]r_cnt_400khz;
     always @(posedge clk, posedge reset_p) begin
@@ -38,16 +42,21 @@ module i2c_top (
         end
     end
     
+    //i2c reg for initialize CLCD
+    //After initialization, keypad input is major
     assign w_addr = (w_empty) ? w_addr_key : w_addr_reg;
     assign w_data = (w_empty) ? w_data_key : w_data_reg;
     assign w_RW = (w_empty) ? w_RW_key : w_RW_reg;
     assign w_valid = (w_empty) ? w_valid_key : w_valid_reg;
     
-    i2c_master_write master(.clk(r_clk_400khz),.reset_p(reset_p),.i_addr(w_addr),.i_data(w_data),.i_RW(w_RW),.i_valid(w_valid),.o_sda(o_sda),. o_scl(o_scl),. o_busy(w_busy));
+    i2c_master master(.clk(r_clk_400khz),.reset_p(reset_p),.i_addr(w_addr),.i_data(w_data),.i_RW(w_RW),.i_valid(w_valid),.o_sda(o_sda),. o_scl(o_scl),. o_busy(w_busy), .r_receive(w_receive));
 
     i2c_reg regi(.clk(clk),.reset_p(reset_p),.i_busy(w_busy),.o_addr(w_addr_reg),.o_data(w_data_reg),.o_RW(w_RW_reg),.o_valid(w_valid_reg),.o_empty(w_empty));
 
     Keypad kpd(.i_clk(clk),.i_reset(reset_p),.i_col(i_col),. o_row(o_row),. o_data(w_data_k2k),.o_btn_valid(w_btn_valid));
     from_keypad_to_i2c_master k2i(.clk(clk), .reset_p(reset_p), .i_data(w_data_k2k), .i_btn_valid(w_btn_valid),
-                                  .i_busy(w_busy), .o_addr(w_addr_key), .o_data(w_data_key), .o_RW(w_RW_key), .o_valid(w_valid_key));
+                                  .i_busy(w_busy), .o_addr(w_addr_key), .o_data(w_data_key), .o_RW(w_RW_key), .o_valid(w_valid_key), .i_receive(w_receive), .o_char_addr(w_value));
+
+    FND_4digit_ctrl fnd(.clk(clk), .reset_p(reset_p), .value({8'b0, w_value}), .com(an), .seg_7(seg));
+    
 endmodule
